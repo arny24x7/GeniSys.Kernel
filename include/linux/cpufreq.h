da83ecbdd3aa3db1c19724c2e5e49af65cfe236f
Arnab Chaudhuri <coolguyforgenerationx@gmail.com>
2016-01-06 13:54:51 +0530

    cpufreq: SmartAss v2: Fix compilation for default governor settings

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -394,6 +394,9 @@ extern struct cpufreq_governor cpufreq_gov_intelliactive;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_WHEATLEY)
 extern struct cpufreq_governor cpufreq_gov_wheatley;
 #define CPUFREQ_DEFAULT_GOVERNOR  (&cpufreq_gov_wheatley)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_SMARTASS2)
+extern struct cpufreq_governor cpufreq_gov_smartass2;
+#define CPUFREQ_DEFAULT_GOVERNOR  (&cpufreq_gov_smartass2)
 #endif
 
 
